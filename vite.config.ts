6bc16640757dc4878aa9aba03d41082a1d95027c
AmirAliNemayandeh <nemayandeha@gmail.com>
2025-11-11 19:31:55 +0330

    Some Another Changes on Vite Config

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/amiri-clock",
+  base: "/amiri-clock/",
 });
